39227cfbad701345efb5927868d3f0e3272227dd
DeividSkii <darlandeivid64@gmail.com>
2026-02-21 17:41:50 -0300

    Projeto JobFinder

---
+++ models/Job.js
@@ -0,0 +1,26 @@
+const Sequelize = require('sequelize')
+const db = require("../db/connection")
+
+const Job = db.define('job', {
+    title: {
+        type: Sequelize.STRING,   
+    },
+    description: {
+        type: Sequelize.STRING,
+    },
+    salary: {
+        type: Sequelize.STRING
+    },
+    company: {
+        type: Sequelize.STRING,
+    },
+    email: {
+        type: Sequelize.STRING
+    },
+    new_job: {
+        type: Sequelize.INTEGER,
+        defaultValue: 1
+    }
+});
+
+module.exports = Job
